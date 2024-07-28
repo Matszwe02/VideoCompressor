0099d8b5a528e0588002364ca792503f737af1e9
Matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2024-07-28 13:35:12 +0200

    File dialog implementation

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import atexit
 import shlex
 import uuid
 import colors
-
+from tkinter import filedialog
 
 
 # codec - h264 or libx265
@@ -24,39 +24,6 @@ extension = 'mp4'
 command = f'-vcodec {codec} -acodec aac -crf {crf}'
 
 
-print(f"using default params: {colors.green(command)}")
-print(colors.cyan("(Ctrl + C to set custom)"))
-try:
-    time.sleep(5)
-    customparams = 'N'
-except KeyboardInterrupt:
-    customparams = 'y'
-
-
-
-if customparams.lower() == 'y':
-    print(f"Type {colors.green('0-51')} to set {colors.cyan('CRF')}. Type {colors.green('h')} to set {colors.cyan('h264')}. Type {colors.green('l')} to set {colors.cyan('libx265')}. Type {colors.green('c')} to set custom command")
-    x = input(colors.green('> '))
-    
-    if x == 'h': codec = 'h264'
-    elif x == 'l': codec = 'libx265'
-    elif x == 'c': pass
-    else:
-        crf = int(x)
-        
-    command = f'-vcodec {codec} -acodec aac -crf {crf}'
-    
-    if x == 'c':
-        print(f'default extension: {colors.green(extension)}')
-        new_extension = input("Video final extension: ")
-        if new_extension.__len__() > 1: extension = new_extension
-        print(f'default command: {colors.green(command)}')
-        new_command = input("command: ")
-        if new_command.__len__() > 1: command = new_command
-    
-    print('New command: ' + colors.green(command))
-
-print('\n\n')
 
 def get_num_frames(video_file):
     try:
@@ -108,12 +75,6 @@ def compress_file(video_file):
             print(colors.red('warning! PermissionError. Retrying in 5s...'))
             time.sleep(5)
 
-time.sleep(0.2)
-
-args = sys.argv[1:]
-
-videos = []
-
 
 def find_videos(directory):
     videos = []
@@ -123,16 +84,60 @@ def find_videos(directory):
                 videos.append(os.path.join(root, file))
     return videos
 
+if __name__ == "__main__":
 
-for arg in args:
-    if os.path.isdir(arg):
-        videos.extend(find_videos(arg))
-    elif os.path.isfile(arg) and arg.split('.')[-1] in ['mp4', 'avi', 'mkv', 'mpg', 'mpeg', 'wmv', 'mov', 'webm']:
-        videos.append(arg)
+    args = sys.argv[1:]
+    if len(args) == 0:
+        file_paths = filedialog.askopenfilenames(filetypes=[("Video Files", "*.mp4 *.avi *.mkv *.mpg *.mpeg *.wmv *.mov *.webm"), ("All files", "*")])
+        for file in file_paths:
+            args.append(file)
+
+    print(f"using default params: {colors.green(command)}")
+    print(colors.cyan("(Ctrl + C to set custom)"))
+    try:
+        time.sleep(5)
+        customparams = 'N'
+    except KeyboardInterrupt:
+        customparams = 'y'
 
 
-for i, vid in enumerate(videos):
-    print(f'Compressing {colors.green(i+1)} of {colors.green(videos.__len__())}')
-    video_file = Path(vid)
-    compress_file(video_file.absolute().__str__())
+    if customparams.lower() == 'y':
+        print(f"Type {colors.green('0-51')} to set {colors.cyan('CRF')}. Type {colors.green('h')} to set {colors.cyan('h264')}. Type {colors.green('l')} to set {colors.cyan('libx265')}. Type {colors.green('c')} to set custom command")
+        x = input(colors.green('> '))
         
+        if x == 'h': codec = 'h264'
+        elif x == 'l': codec = 'libx265'
+        elif x == 'c': pass
+        else:
+            crf = int(x)
+            
+        command = f'-vcodec {codec} -acodec aac -crf {crf}'
+        
+        if x == 'c':
+            print(f'default extension: {colors.green(extension)}')
+            new_extension = input("Video final extension: ")
+            if new_extension.__len__() > 1: extension = new_extension
+            print(f'default command: {colors.green(command)}')
+            new_command = input("command: ")
+            if new_command.__len__() > 1: command = new_command
+        
+        print('New command: ' + colors.green(command))
+
+    print('\n\n')
+    
+    time.sleep(0.2)   
+    videos = []
+
+
+    for arg in args:
+        if os.path.isdir(arg):
+            videos.extend(find_videos(arg))
+        elif os.path.isfile(arg) and arg.split('.')[-1] in ['mp4', 'avi', 'mkv', 'mpg', 'mpeg', 'wmv', 'mov', 'webm']:
+            videos.append(arg)
+
+
+    for i, vid in enumerate(videos):
+        print(f'Compressing {colors.green(i+1)} of {colors.green(videos.__len__())}')
+        video_file = Path(vid)
+        compress_file(video_file.absolute().__str__())
+            
